5bb1a5f82eb25cfba4683bd78fa2ab129e473924
Carlos Alarcon <jchuerva@gmail.com>
2023-06-09 23:39:36 +0200

    use malaga score for game

--- 3. arregla_la grafica.rb
+++ 3. arregla_la grafica.rb
@@ -1,4 +1,4 @@
-# Sound Graphs: goles del Malaga
+# Sound Graphs: puntos del Malaga
 require 'csv'
 
 # Carga los datos del fichero
@@ -7,12 +7,16 @@ data = CSV.read("/Users/jchuerva/Desktop/Sonic-pi/Malaga-21_22.csv")
 
 use_synth :piano
 
+# Puntos
+# perder - 0 punto
+# empatar - 1 punto
+# ganar - 3 puntos
+
+score = 0
 data.each do |row|
   
-  goals = row[0].to_i
-  play 50
+  score = score + row[1].to_i
+  play 40
   
   sleep 0.5
 end
-
-
