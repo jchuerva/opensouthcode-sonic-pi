b5c3247e048d325f61e6bf7f73c322f4e5eb8fbd
Carlos Alarcon <jchuerva@github.com>
2023-06-09 23:26:39 +0200

    Add files via upload

---
+++ 1. arregla_la_cancion_solucion.rb
@@ -0,0 +1,33 @@
+# Arregla la cancion (solucion)
+
+Do = :C5
+Re = :D5
+Mi = :E5
+Fa = :F5
+Sol = :G5
+La = :A5
+Si = :B5
+
+play Do #C5
+sleep 0.5
+play Do #C5
+sleep 0.5
+play Re #D5
+sleep 0.5
+play Do #C5
+sleep 0.5
+play Fa #F5
+sleep 0.5
+play Mi #E5
+sleep 0.5
+play Do #:C5
+sleep 0.5
+play Do #:C5
+sleep 0.5
+play Re #:D5
+sleep 0.5
+play Do #:C5
+sleep 0.5
+play Sol #:G5
+sleep 0.5
+play Fa #:F5
---
+++ 2. arregla_la_banda_solucion.rb
@@ -0,0 +1,57 @@
+##| Arregla la banda! (solucion)
+
+use_bpm 120
+
+live_loop :rummut do
+  ##| stop
+  sample :drum_heavy_kick
+  sleep 1
+  sample :drum_snare_hard
+  sleep 1
+  sample :drum_heavy_kick
+  sleep 1
+  sample :drum_snare_hard
+  sleep 1
+end
+
+live_loop :hihat do
+  ##| stop
+  sample :drum_cymbal_closed
+  sleep 0.25
+  sample :drum_cymbal_pedal
+  sleep 1
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+live_loop :basso do
+  stop
+  use_synth :fm
+  play :c2, attack: 0, release: 0.25
+  sleep 0.25
+  play :c2, attack: 0, release: 0.3
+  sleep 2
+  play :e2
+  sleep 0.75
+  play :f2
+  sleep 1
+end
+
+live_loop :melodia do
+  stop
+  play_pattern_timed [:c4, :e4, :f4, :g4, :f4, :e4, :f4, :g4, :f4, :e4, :f4], [0.25, 0.25, 0.25, 1.5, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25], attack: 0, release: 0.2
+end
